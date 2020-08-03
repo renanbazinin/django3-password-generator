2e88da2ed091a5884368aac7d9ab3a42a3d52094
renan <renanbazinin2@gmail.com>
2020-08-03 19:04:22 +0300

    V1

---
+++ generator/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import random
+
+def home(request):
+    return render(request,'generator/home.html',{'password':'1234'})
+
+
+def password(request):
+
+
+
+    chars = list('abcdefghjklmopqrstuvwyz')
+
+    if request.GET.get('uppercase'):
+        chars.extend(list('QWERTYUIOPASDFGHJKLZXCVBNM'))
+    if request.GET.get('numbers'):
+        chars.extend(list('0123456789'))
+    if request.GET.get('spechar'):
+        chars.extend(list('!@#$%^&*()'))
+
+    length = int(request.GET.get('length'))
+
+    thepassword = ''
+    for x in range(length):
+        thepassword+=random.choice(chars)
+
+    return render(request,'generator/generator.html',{'password':thepassword})
+
+
+def about(request):
+    return render(request, 'generator/aboutus.html')
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\renan\Documents\I Want to lean Django\password_generator\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
